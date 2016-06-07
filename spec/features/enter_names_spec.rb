8c018ff190fcb14af45a3c4237a50f42f00538de
Anna <anna.hollandsmith3@gmail.com>
2016-06-07 21:19:32 +0100

    challenge 20 complete

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -8,4 +8,14 @@ RSpec.feature 'Enter names' do
     click_button 'Submit'
     expect(page).to have_content 'Anna vs. Luke'
   end
+
+  feature 'View hit points' do
+    scenario 'see hit points for player two' do
+      visit('/')
+      fill_in :player_1_name, with: 'Anna'
+      fill_in :player_2_name, with: 'Luke'
+      click_button 'Submit'
+      expect(page).to have_content 'Luke: 60HP'
+    end
+  end
 end

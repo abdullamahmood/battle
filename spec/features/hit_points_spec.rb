5b04092b56bb568e57e2c713eb4eeda7f9c395c2
Abdulla <abdulla.ali.mahmood@gmail.com>
2016-06-09 17:28:52 +0100

    29 almost done

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -5,5 +5,14 @@ feature "Viewing player 2's Hit Points " do
 		sign_in_and_play
 		expect(page).to have_content "Abdulla: 100 HP"
 	end
-
+end	
+feature "Give a lose message when hit point of the losing player is 0" do
+	scenario "See player 2 losing message" do
+	sign_in_and_play
+	19.times do
+		click_button "Attack"
+		click_button "Switch turns"
+	end
+	expect(page).to have_content "Abdulla loses"
+	end
 end

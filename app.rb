ef30baf3fef709c1800e7026f9a0c65b35c55f23
Graham Ormiston <grahamormiston@gmail.com>
2016-06-09 14:30:03 +0100

    #26: Skinny controllers

--- app.rb
+++ app.rb
@@ -11,23 +11,18 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-  	$player_1 = Player.new(params[:player_1_name])
-  	$player_2 = Player.new(params[:player_2_name])
-  	# p params
+    $game = Game.new(Player.new(params[:player_1_name]),Player.new(params[:player_2_name]))
   	redirect '/play'
   end
 
   get '/play' do
-    @player_1 = $player_1
-    @player_2 = $player_2
+    @game = $game
     erb :play
   end
 
   get '/attack' do
-  	@player_1 = $player_1
-    @player_2 = $player_2
-    @game = Game.new
-    @game.attack(@player_2)
+    @game = $game
+    @game.attack(@game.player_2)
     erb :attack
   end
 
